472133c4eb9b12fc699802f8f31ee005da881101
AnnaChang <annachang@kkbox.com>
2014-06-16 14:14:41 +0800

    add a new file:name-score.py

--- name-score.py
+++ name-score.py
@@ -1,17 +1,20 @@
+
+# coding: utf-8
+
+# In[69]:
+
 filled_dict = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5,"F": 6,"G": 7,"H": 8,"I": 9,"J":10,"K": 11,"L": 12}
-list1=["CDEF","ABC","FIJK"]
-list1=sorted(list1)
+List1=["CDEF","ABC","FIJK"]
+List1=sorted(list1)
 newlist=[]
-print list1
-L1 =['ABC', 'CDEF', 'FIJK']
-L2 = list("".join(L1))
-print L2
-for i in L2:
+print List1
+List1 =['ABC', 'CDEF', 'FIJK']
+List2 = list("".join(List1))
+print List2
+for i in List2:
     x=filled_dict [i]
     newlist.append(x)
     print newlist
-    z=reduce (lambda x , y : x + y , range (0,len(newlist)))
+    z=sum(newlist)
     print z
-
-
 
